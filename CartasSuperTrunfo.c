cb5b4bfa544256778527fc9a40ac58f81c58fdb9
MuscleDev <rmuscledev@gmail.com>
2025-05-21 01:48:25 +0000

    Aplicação Finalizada e Testada.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,37 +14,37 @@ int main() {
     printf("Você deverá cadastrar sua carta colocando informações sobre a cidade escolhida!\n");
 // Aqui utilizei a função printf para exibir no terminal instruções do funcionamento do jogo antes do usuário começar a cadastrar suas cartas.
 
-    printf("Qual estado da cidade? \n");
+    printf("Qual estado da cidade 1? \n");
     scanf("%c", &estado1);
-    printf("Qual o código da carta? \n");
+    printf("Qual o código da carta 1? \n");
     scanf("%s", codigo1);
-    printf("Qual o nome da cidade? \n");
+    printf("Qual o nome da cidade 1? \n");
     scanf("%s", cidade1);
-    printf("Qual a população da cidade? \n");
+    printf("Qual a população da cidade 1? \n");
     scanf("%d", &populacao1);
-    printf("Qual a área da cidade? \n");
+    printf("Qual a área da cidade 1? \n");
     scanf("%f", &area1);
-    printf("Qual o PIB per capita da cidade? \n");
+    printf("Qual o PIB per capita da cidade 1? \n");
     scanf("%f", &pib1);
-    printf("Quantos pontos turísticos existem na cidade? \n");
+    printf("Quantos pontos turísticos existem na cidade 1? \n");
     scanf("%d", &turisticos1);
-
-    printf("Qual estado da cidade? \n");
+    printf("Qual o estado da cidade 2? \n");
     scanf("%c", &estado2);
-    printf("Qual o código da carta? \n");
+    printf("Qual o código da carta 2? \n");
     scanf("%s", codigo2);
-    printf("Qual o nome da cidade? \n");
+    printf("Qual o nome da cidade 2? \n");
     scanf("%s", cidade2);
-    printf("Qual a população da cidade? \n");
+    printf("Qual a população da cidade 2? \n");
     scanf("%d", &populacao2);
-    printf("Qual a área da cidade? \n");
+    printf("Qual a área da cidade 2? \n");
     scanf("%f", &area2);
-    printf("Qual o PIB per capita da cidade? \n");
+    printf("Qual o PIB per capita da cidade 2? \n");
     scanf("%f", &pib2);
-    printf("Quantos pontos turísticos existem na cidade? \n");
+    printf("Quantos pontos turísticos existem na cidade 2? \n");
     scanf("%d", &turisticos2);
-// Aqui foi montado uma serie de questões feitas ao usuário utilizando da função printf e para coletar a resposta foi utilizada a função scanf
-    printf("Carta 1\n");
+    
+// Aqui foi montado uma serie de questões feitas ao usuário utilizando da função printf e para coletar a resposta foi utilizada a função scanf atentando ao tipo de variavel a ser solicitada.
+    printf("Carta 1:\n");
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigo1);
     printf("Nome da cidade: %s\n", cidade1);
@@ -53,8 +53,7 @@ int main() {
     printf("PIB per capita: %.2f mil reais\n", pib1);
     printf("Número de pontos turísticos: %d\n", turisticos1);
     printf("\n");
-
-    printf("Carta 2\n");
+    printf("Carta 2:\n");
     printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codigo2);
     printf("Nome da cidade: %s\n", cidade2);
@@ -62,6 +61,7 @@ int main() {
     printf("Área: %.2f km²\n", area2);
     printf("PIB per capita: %.2f mil reais\n", pib2);
     printf("Número de pontos turísticos: %d\n", turisticos2);
+    //Por fim foi ultilizada a função printf para mostrar no terminal os dados inseridos pelo usuário e exibidos de forma organizada um em cada linha.
 
     return 0;
 }
